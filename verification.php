8d4247ea503caf6e73e36a5a2ace7d4b34b7127a
LaurieFer <laurie.fernanli@gmail.com>
2016-10-07 19:52:50 +0200

    Update verification.php

--- verification.php
+++ verification.php
@@ -4,9 +4,12 @@
 </head>
 <body>
 
-<form sauvegarde.php>
-<input type="submit" value="Modifier un exercice ">
-<input type="submit" value="Valider et sauvegarder">
+<form>
+<input type="" name="modifier" value="Modifier un exercice ">
+<input type=""  value="Valider et sauvegarder">
 </form>
+  <?php
+  if($_GET['Modifier']
+  ?>
 </body>
 </html>
