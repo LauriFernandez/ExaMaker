4bbaa2a73488614e25a09fb9d12893041efc7936
LaurieFer <laurie.fernanli@gmail.com>
2016-10-07 19:48:57 +0200

    Create verification.php

---
+++ verification.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+
+</head>
+<body>
+
+<form sauvegarde.php>
+<input type="submit" value="Modifier un exercice ">
+<input type="submit" value="Valider et sauvegarder">
+</form>
+</body>
+</html>
