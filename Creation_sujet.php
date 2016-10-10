96337204d6bc7f2bc8d82566dd923195e1d4df73
AssanDiomande <assanivory@gmail.com>
2016-10-10 22:11:31 +0200

    Add files via upload

---
+++ Creation_sujet.php
@@ -0,0 +1,28 @@
+<?php session_start() ?>
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Création du sujet</title>
+</head>
+
+<body>
+	<header>
+		<h1>Création du sujet</h1> 
+	</header>
+
+<br/><br/>	
+	
+	<div>
+	
+			
+		<a href="detailExo.php">Voir le contenu du sujet</a><br/>
+	
+		
+		<br/>
+		<a href="intro.php"><button> creer un exercice </button></a> <tr/> <button>sauvegarder et valider le sujet</button> <tr/> <button>modifier un exercice</button>
+	</div>
+
+
+</body>
+</html>
