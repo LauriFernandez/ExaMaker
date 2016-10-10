d6e4257481da1702089cf152714cc1e1c0752c42
LaurieFer <laurie.fernanli@gmail.com>
2016-10-10 16:24:18 +0200

    Update verification.php

--- verification.php
+++ verification.php
@@ -3,8 +3,7 @@
 
 </head>
 <body>
-
-<form> //permet au créateur de faire le choix entre modifier ou sauvegarder le sujet
+<form>  <!--permet au créateur de faire le choix entre modifier ou sauvegarder le sujet -->
 <input type="radio" name="choix" value="Modifier un exercice ">
 <input type="radio" name="choix" value="Sauvegarder">
   <input type="submit" value="Valider mon choix">
