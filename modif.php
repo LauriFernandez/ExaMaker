7f7c94456b0b5862a42d3a8d49cb8ef98f7cda05
LaurieFer <laurie.fernanli@gmail.com>
2016-10-10 16:22:17 +0200

    Add files via upload

---
+++ modif.php
@@ -0,0 +1,14 @@
+<html>
+<head>  
+<title> <?php echo 'Premiers pas en PHP'; ?> </title>
+  <meta charset="utf-8"/>
+</head>
+
+<body>
+<h1> Modification</h1>
+<?php
+echo "<p> Votre modification est en cours </p>";
+
+?>
+</body>
+</html>
---
+++ sauvegarde.php
@@ -0,0 +1,14 @@
+<html>
+<head>  
+<title> <?php echo 'Premiers pas en PHP'; ?> </title>
+  <meta charset="utf-8"/>
+</head>
+
+<body>
+<h1> Sauvegarde en cours...</h1>
+<?php
+echo "<p> Votre sauvegarde est en cours...</br> Veuillez Patientez...</p>";
+
+?>
+</body>
+</html>
