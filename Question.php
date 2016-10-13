2840678474fdf271ce50957000803f489825c669
AssanDiomande <assanivory@gmail.com>
2016-10-13 18:03:32 +0200

    Update Question.php

--- Question.php
+++ Question.php
@@ -3,7 +3,7 @@
 <html>
 <head>
 	<meta charset="utf-8"/>
-	<title>Création du sujet</title>
+	<title>Création des questions</title>
 </head>
 
 <body>
@@ -15,10 +15,9 @@
 	
 	<div>
 	
-	<?php $indiceExo=$_SESSION['ind']; ?>
-	
 	<form method="post" action="Creation_exo.php">
 		<p> Numero de la question: <input type="text" name="num"/> </p>
+		<p> Enoncé: <textarea col="10" row="10" name="énoncé"></textarea> </p>
 		<p> Question: <input type="text" name="question"/> </p>	
 		<p> Reponse: <textarea col="10" row="10" name="reponse"></textarea> </p>
 		<p> Bareme: <input name="bareme"/></p>
