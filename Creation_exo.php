205a9a52a9eeea7dfde5d5a309d08ed298795f05
AssanDiomande <assanivory@gmail.com>
2016-10-13 18:01:54 +0200

    Update Creation_exo.php

--- Creation_exo.php
+++ Creation_exo.php
@@ -3,12 +3,12 @@
 <html>
 <head>
 	<meta charset="utf-8"/>
-	<title>Création du sujet</title>
+	<title>Choix des questions</title>
 </head>
 
 <body>
 	<header>
-		<h1>Création de l'exercice</h1> 
+		<h1>Choix des questions</h1> 
 
 	</header>
 
@@ -18,14 +18,17 @@
 		
 		<?php
 			
+			echo "Voulez vous ajouter les elements suivants à l'exercice numero: ".$_SESSION['ind'].' ?'.'<br/>';
 			
-			
-			if(isset($_POST['num']) and isset($_POST['question']) and isset($_POST['reponse']) and isset($_POST['bareme']))
+			if(isset($_POST['num']) and isset($_POST['question']) and isset($_POST['reponse']) and isset($_POST['bareme']) and isset($_POST['énoncé']))
 			{	
+				$_SESSION['nbQuestion'][$_SESSION['ind']]++;
 				
-				if(!isset($_SESSION['ex'.$_SESSION['ind']]['bareme']) and !isset($_SESSION['ex'.$_SESSION['ind']]['num']) and !isset($_SESSION['ex'.$_SESSION['ind']]['question']) and !isset($_SESSION['ex'.$_SESSION['ind']]['reponse']))
+				if(!isset($_SESSION['ex'.$_SESSION['ind']]['énoncé']) and !isset($_SESSION['ex'.$_SESSION['ind']]['bareme']) and !isset($_SESSION['ex'.$_SESSION['ind']]['num']) and !isset($_SESSION['ex'.$_SESSION['ind']]['question']) and !isset($_SESSION['ex'.$_SESSION['ind']]['reponse']))
 				{
+					
 					$_SESSION['ex'.$_SESSION['ind']]['num'] = array();
+					$_SESSION['ex'.$_SESSION['ind']]['énoncé'] = array();
 					$_SESSION['ex'.$_SESSION['ind']]['question'] = array();
 					$_SESSION['ex'.$_SESSION['ind']]['reponse'] = array();
 					$_SESSION['ex'.$_SESSION['ind']]['bareme']=array();					
@@ -33,25 +36,23 @@
 				
 				$i=(int)$_POST['num'];
 				
-				$_SESSION['ex'.$_SESSION['ind']]['num'][$i]=$_POST['num'];			
+				$_SESSION['ex'.$_SESSION['ind']]['num'][$i]=$_POST['num'];	
 				$_SESSION['ex'.$_SESSION['ind']]['question'][$i]=$_POST['question'];
+				$_SESSION['ex'.$_SESSION['ind']]['énoncé'][$i]=$_POST['énoncé'];
 				$_SESSION['ex'.$_SESSION['ind']]['reponse'][$i]=$_POST['reponse'];
-				$_SESSION['ex'.$_SESSION['ind']]['bareme'][$i]=$_POST['bareme'];				
-				
-		
-					echo '<strong>'."Exercice numero: ".$_SESSION['ind'].'</strong>'.'<br/><br/>';
+				$_SESSION['ex'.$_SESSION['ind']]['bareme'][$i]=$_POST['bareme'];									
 					
-					
-						echo "Question numero: ".$_SESSION['ex'.$_SESSION['ind']]['num'][$i].'<br/>';	
+						echo "Question numero: ".$_SESSION['ex'.$_SESSION['ind']]['num'][$i].'<br/>';
+						echo "énoncé: ".$_SESSION['ex'.$_SESSION['ind']]['question'][$i].'<br/>';
 						echo "Question: ".$_SESSION['ex'.$_SESSION['ind']]['question'][$i].'<br/>';
-						echo "Reponse: ".$_SESSION['ex'.$_SESSION['ind']]['reponse'][$i].'<br>';
+						echo "Reponse: ".$_SESSION['ex'.$_SESSION['ind']]['reponse'][$i].'<br/>';
 						echo "bareme: ".$_SESSION['ex'.$_SESSION['ind']]['bareme'][$i].'<br/><br/>';
 					
 												
 			}
 		?>
 		
-		<a href="Question.php"><button>ajouter une question</button></a> <tr/> <a href="Creation_sujet.php"><button>valider l'exercice</button></a>
+		<a href="Creation_sujet.php"><button>valider</button></a> <tr/> <a href="Question.php"><button>modifier</button></a>
 		
 		<br/><br/>
 	
