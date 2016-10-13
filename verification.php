454cb7bce3fe9ec0729e564899480e0343b366fd
LaurieFer <laurie.fernanli@gmail.com>
2016-10-13 20:10:35 +0200

    Add files via upload

--- verification.php
+++ verification.php
@@ -1,24 +1,30 @@
 <html>
-<head>
-
+<head>  
+<title> <?php echo 'Premiers pas en PHP'; ?> </title>
+  <meta charset="utf-8"/>
 </head>
-<body>
-<form>  <!--permet au créateur de faire le choix entre modifier ou sauvegarder le sujet -->
-<input type="radio" name="choix" value="Modifier un exercice ">
-<input type="radio" name="choix" value="Sauvegarder">
-  <input type="submit" value="Valider mon choix">
-</form>
 
-<?php
-if($_GET['choix']=='modifier')
+<body>
+
+
+<form > <!-- permet au créateur de faire le choix entre modifier ou sauvegarder le sujet-->
+
+<input type="submit" name="choix" value="m">
+<input type="submit" name="choix" value="S">
+</form>
+<?php 
+if(isset($_GET['choix']))
+{
+if($_GET['choix']=='s')
 {
 header(' Location : http://localhost/~11503023/php/exo1.php');
 exit();
-};
-else {
+}
+
+else{
 header('Location: http://localhost/~11503023/projetS3/sauvegarde.php');
 exit();
-};
-
+}
+}
 ?>
 </html>
