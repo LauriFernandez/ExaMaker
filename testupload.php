1220d199d04c047f67b004339f35a5b9925e9c24
brian-bourdon <brian78.b@gmail.com>
2016-11-05 16:11:58 +0100

    Add files via upload

---
+++ testupload.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="testupload.css">  
+  <meta charset="utf-8">
+</head>
+<body>
+    <form enctype="multipart/form-data" action="verif_exam.php" method="post">
+      <input type="hidden" name="MAX_FILE_SIZE" value="100000" />
+      Charger l'examen : <input  type="file" name="fichier" value="charger" />
+      <input  type="submit"  name="charger" value="charger" />
+    </form>
+</body>
+</html>
