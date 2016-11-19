203052cc52680621801d17a2054d6246116f96cc
brian-bourdon <brian78.b@gmail.com>
2016-11-19 15:20:36 +0100

    Add files via upload

--- verif_exam.php
+++ verif_exam.php
@@ -25,7 +25,7 @@ function test_exam()
 	$j = 0;
 	$k = 0;
 	$count_fin = 0;
-	$fich = @fopen($_FILES['fichier']['name'], 'r');
+	$fich = @fopen($_FILES['fichier']['tmp_name'], 'r');
 	if($fich)
 	{
 		while(false !== ($char = fgetc($fich))) // tant qu on a pas atteint la fin du fichier
@@ -83,24 +83,41 @@ function test_exam()
 	}
 }
 
-if ($_FILES['fichier']['error'] == 0) 
+$dossier = 'upload/';
+$fichier = basename($_FILES['fichier']['name']);
+$taille_maxi = 100000;
+$taille = filesize($_FILES['fichier']['tmp_name']);
+
+if(!in_array($extensions_up, $extensions_val)) //Si l'extension n'est pas dans le tableau
 {
-	if (in_array($extensions_up,$extensions_val)) // teste si extension valide
-	{	
-		
-			if(test_exam())
-			{
-				//print_r($tab_verif);
-				echo "Upload reussie"; // teste si l'interieur du .exam est correct
-			}
-			else 
-			{
-				echo "Le contenut du fichier ne correspond pas";
-				//print_r($tab_verif);
-			}
-	
-	}
-	else echo 'Extension incorrecte';
+     $erreur = 'Vous devez uploader un fichier de type exam';
 }
-else echo 'Erreur lors du transfert';  
+if($taille>$taille_maxi) // Si taille fichier trop gros
+{
+     $erreur = 'Le fichier est trop gros...';
+}
+if(!test_exam()) // Si la syntaxe du fichier n'est pas celui d'un .exam
+{
+	$erreur = 'Le contenut du fichier ne coresspond pas...';
+}
+if(!isset($erreur)) //S'il n'y a pas d'erreur, on upload
+{
+	 //On formate le nom du fichier(pour gerer erreurs accent etc..)
+     $fichier = strtr($fichier, 
+          'ÀÁÂÃÄÅÇÈÉÊËÌÍÎÏÒÓÔÕÖÙÚÛÜÝàáâãäåçèéêëìíîïðòóôõöùúûüýÿ', 
+          'AAAAAACEEEEIIIIOOOOOUUUUYaaaaaaceeeeiiiioooooouuuuyy');
+     $fichier = preg_replace('/([^.a-z0-9]+)/i', '-', $fichier);
+     if(move_uploaded_file($_FILES['fichier']['tmp_name'], $dossier.$fichier))
+     {
+     	echo 'Upload effectué avec succès !';
+     }
+     else
+     {
+     	echo 'Echec de l\'upload !';
+     }
+}
+else
+{
+     echo $erreur;
+} 
 ?>
